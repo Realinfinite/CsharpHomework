de5040728a2ad8a4a4578c48fcf03adbc2407499
Todorokiii <61154578+Todorokiii@users.noreply.github.com>
2020-02-28 11:46:35 +0800

    Add files via upload

---
+++ HOMEWORK2/Project3/All prime number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[99];
+            for(int i= 0;i<99;i++)
+            {
+                n[i] = i + 2;
+            }
+            List<int> arr_list = n.OfType<int>().ToList();
+            for (int j = 2; j <= 100; j++)
+            {
+                for (int num = 2; num <= 100; num++)
+                {
+                    if (num % j == 0 && num != j)
+                    {
+                        arr_list.Remove(item: num);
+
+                    }
+                }
+            } 
+            foreach (var item in arr_list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();         
+        }
+    }
+}
