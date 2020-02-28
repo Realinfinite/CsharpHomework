a8979318ba11266257d67b70dd28fe85925cc311
Todorokiii <61154578+Todorokiii@users.noreply.github.com>
2020-02-28 11:40:20 +0800

    Add files via upload

---
+++ HOMEWORK2/Project1/Prime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String numresults = " ";
+                int num = Int32.Parse(Console.ReadLine());
+                for (int j = 2; j < num; j++)
+                {
+                    while (num % j == 0 && num != j)
+                    {
+                        num /= j;
+                        numresults += j + ",";
+                    }
+                }
+                numresults += num;
+                Console.Write(numresults);
+            }
+            catch(Exception x)
+            {
+                Console.WriteLine("Put an int");
+            }
+            Console.ReadKey();
+
+        }
+       
+
+    }
+}
