3139c9c611049bee345ee923754ee2a325b07dea
Todorokiii <Luowuqiong17@icloud.com>
2020-03-06 11:17:15 +0800

    message

---
+++ HOMEWORK3/Project1/3th work1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+interface IShape
+    {
+        // 接口成员
+        double CalcuArea();
+        bool TestShape();
+    }
+
+
+
+    //三角形
+    class Triangle : IShape
+    {
+        private double a { set; get; }
+       private double b { set; get; }
+        private double c{ set; get; }
+  
+    public Triangle(double a1, double b1, double c1)
+        {
+            this.a = a1;
+            this.b = b1;
+            this.c = c1;
+        }
+        public double CalcuArea()
+        {
+            double d = (a + b + c) / 2.0;
+            double s = Math.Sqrt(d * (d - a) * (d - b) * (d - c));
+            return s;
+        }
+        public bool TestShape()
+        {
+        return ((a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (c + b) > a) ? true : false);
+           
+        }
+    }
+    //长方形
+    class Rectangle : IShape
+    {
+        private  double a { set; get; }
+    public double b { set; get; }
+      
+        public Rectangle(double a1, double b1)
+        {
+            this.a = a1;
+            this.b = b1;
+        }
+
+        public bool TestShape()
+        {
+        return (a != b ? true : false);
+          
+        }
+        public double CalcuArea()
+        {
+            double s = a * b;
+            return s;
+        }
+
+    }
+    //正方形
+    class Square : IShape {
+       public double a { get; set; }
+
+        public Square(double a1)
+        {
+            this.a = a1;
+
+        }
+        public bool TestShape()
+        {
+            return true;
+        }
+        public double CalcuArea()
+        {
+            double s = a * a;
+            return s;
+        }
+    }
+    class Create
+    {
+        public void CreateTri()
+        {
+
+            string s1 = Console.ReadLine();
+            double a = Convert.ToDouble(s1);
+            string s2 = Console.ReadLine();
+            double b = Convert.ToDouble(s2);
+            string s3 = Console.ReadLine();
+            double c = Convert.ToDouble(s3);
+            Triangle tri = new Triangle(a, b, c);
+
+            if (tri.TestShape())
+            {
+                Console.WriteLine("该图形的面积是" + tri.CalcuArea());
+            }
+            else
+                Console.WriteLine("两边不合法，无法构造三角形");
+        }
+        public void CreateRec()
+        {
+
+            string s1 = Console.ReadLine();
+            double a = Convert.ToDouble(s1);
+            string s2 = Console.ReadLine();
+            double b = Convert.ToDouble(s2);
+            Rectangle rec = new Rectangle(a, b);
+            if (rec.TestShape())
+                Console.WriteLine("该图形的面积是" + rec.CalcuArea());
+
+            else
+                Console.WriteLine("两边不合法，无法构造长方形");
+        }
+        public void CreateSqu()
+        {
+
+            string s1 = Console.ReadLine();
+            double a = Convert.ToDouble(s1);
+            Square squ = new Square(a);
+            Console.WriteLine("该图形的面积是" + squ.CalcuArea());
+        }
+
+    }
+    class program
+    {
+        static void Main()
+        {
+            Create c = new Create();
+            Console.WriteLine("输入三角形三边长");
+            c.CreateTri();
+            Console.WriteLine("输入长方形两边长");
+            c.CreateRec();
+            Console.WriteLine("输入正方形一边长");
+            c.CreateSqu();
+        }
+    }
+
+
+
---
+++ HOMEWORK3/Project2/3th work2.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+interface IShape {
+    double CalcuArea();
+    bool TestShape();
+
+}
+
+class Triangle : IShape
+{
+    private double a { set; get; }
+    private double b { set; get; }
+    private double c { set; get; }
+
+    public Triangle(double a1, double b1, double c1)
+    {
+        try {
+            this.a = a1;
+            this.b = b1;
+            this.c = c1;
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("不合法");
+        }
+    
+    }
+    public double CalcuArea()
+    {
+      
+            double d = (a + b + c) / 2.0;
+            return (Math.Sqrt(d * (d - a) * (d - b) * (d - c)));
+    
+    }
+    public bool TestShape()
+    {
+        return ((a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (c + b) > a) );
+
+    }
+    class Rectangle : IShape
+    {
+        private double a { set; get; }
+        private double b { set; get; }
+
+        public Rectangle(double a1, double b1)
+        {
+            try
+            {
+                this.a = a1;
+                this.b = b1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("不合法");
+            }
+        }
+
+        public bool TestShape()
+        {
+            return (a != b&&a>0&&b>0);
+        }
+        public double CalcuArea()
+        {
+
+            return (a * b);
+        }
+
+    }
+    class Square : IShape
+    {
+        private double a { get; set; }
+
+        public Square(double a1)
+        {
+            try
+            {
+                this.a = a1;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("不合法");
+            }
+        }
+        public bool TestShape()
+        {
+            return (a > 0 );
+        }
+        public double CalcuArea()
+        {
+            return (a * a);
+        }
+
+    }
+    class ShapeFactory
+    {
+
+
+        public double Selection(int n)
+        {
+            Random ra = new Random();
+            bool flag = false;
+            double s = 0;
+
+            switch (n)
+            {
+                case 0:
+                    while (!flag)
+                    {
+                        Triangle tri = new Triangle(ra.Next(1, 100), ra.Next(1, 100), ra.Next(1, 100));
+                        s = tri.CalcuArea();
+                        flag = tri.TestShape();
+                    }
+                    Console.WriteLine("随机构造三角形，其面积为" + Math.Round(s, 2));
+                    break;
+                case 1:
+                    while (!flag)
+                    {
+                        Rectangle rec = new Rectangle(ra.Next(1, 100), ra.Next(1, 100));
+                        s = rec.CalcuArea();
+                        flag = rec.TestShape();
+                    }
+                    Console.WriteLine("随机构造矩形，其面积为" + Math.Round(s, 2));
+                    break;
+                case 2:
+                    while (!flag)
+                    {
+                        Square squ = new Square(ra.Next(1, 100));
+                        s = squ.CalcuArea();
+                        flag = squ.TestShape();
+                    }
+                    Console.WriteLine("随机构造正方形，其面积为" + Math.Round(s, 2));
+                    break;
+                default:
+                    break;
+            }
+            return (Math.Round(s, 2));
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            ShapeFactory shapefac = new ShapeFactory();
+            double total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Random ra = new Random();
+                int n = ra.Next(1, 100);
+                int m = n % 3;
+                total += shapefac.Selection(m);
+            }
+            Console.WriteLine("随机构造图形的总面积为" + total);
+        }
+    }
+}
