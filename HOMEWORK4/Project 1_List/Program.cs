3acc30ae792bedf902d3f75f560ce2ad0ca4b116
Todorokiii <Luowuqiong17@icloud.com>
2020-03-13 10:50:00 +0800

    Homework4

---
+++ HOMEWORK4/Project 1_List/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void EachNode(Action<T> action)
+        {
+            Node<T> node = head;
+            if (node == null) return;
+            action(node.Data);
+            while (node != tail) { 
+                node = node.Next;
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+           /* for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }*/
+           intlist.EachNode( x =>  Console.WriteLine(x+" ") );
+            int sum = 0; int max = 0; int min = 0;
+            intlist.EachNode(x => sum += x);
+            Console.WriteLine(sum);
+            intlist.EachNode(x => max = x > max ? x : max);
+            Console.WriteLine(max);
+            intlist.EachNode(x => min = x < min ? x : min);
+            Console.WriteLine(min);
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+
+        }
+
+    }
+}
---
+++ HOMEWORK4/Project2_Clock/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+
+    public delegate void Clock();
+
+    public class Tick_Alarm
+    {
+        public event Clock OnTick, OnAlarm;
+        public int time = 0;
+        public void DoTick(int i)
+        {
+                if (i % 10 == 0 && OnTick != null)
+                    OnTick();
+            
+        }
+        public void DoAlarm(int i)
+        {
+            
+                if(i%30 ==0&&OnAlarm !=null)
+                OnAlarm();
+            
+        }
+    }
+
+    public class Form
+    {
+        public Tick_Alarm tick_alarm = new Tick_Alarm();
+
+        public Form()
+        {
+            tick_alarm.OnTick += Clock_Tick;
+            tick_alarm.OnAlarm += Clock_Alarm;
+
+        }
+        void Clock_Tick()
+        {
+            Console.WriteLine("Clock is ticking!");
+        }
+
+        void Clock_Alarm()
+        {
+            Console.WriteLine("Clock is alarming!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            for(int i = 0; i < 100; i++)
+            {
+                form1.tick_alarm.DoAlarm(i);
+                form1.tick_alarm.DoTick(i);
+            }
+         
+        }
+    }
+}
