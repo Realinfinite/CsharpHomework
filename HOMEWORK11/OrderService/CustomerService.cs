0dcde71576dbda1827f6fe9ee323b639a0abb467
Todorokiii <luowuqiong17@icloud.com>
2020-05-24 15:09:23 +0800

    message

---
+++ HOMEWORK11/OrderService/CustomerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp {
+  public class CustomerService {
+
+    public static void Add(Customer customer) {
+      try {
+        using (var db = new OrderContext()) {
+          db.Customers.Add(customer);
+          db.SaveChanges();
+        }
+      }
+      catch (Exception e) {
+       
+        throw new ApplicationException($"Add wrong!");
+      }
+    }
+
+    public static List<Customer> GetAll() {
+      using (var db = new OrderContext()) {
+        return db.Customers.ToList();
+      }
+    }
+  }
+
+}
